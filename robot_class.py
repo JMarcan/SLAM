0e121f14b3e7415e81826007d6f383b3d988df7d
JMarcan <jan.marcan@protonmail.com>
2020-07-04 12:42:16 +0200

    fix: measurements in sense function to work even with negative values for dx and dy instead of storing just their absolute value

--- robot_class.py
+++ robot_class.py
@@ -76,8 +76,6 @@ class robot:
             This function should account for measurement_noise and measurement_range.
             One item in the returned list should be in the form: [landmark_index, dx, dy].
             '''
-           
-        measurements = None
         
         ## TODO: iterate through all of the landmarks in a world
         
@@ -94,31 +92,19 @@ class robot:
         
         measurements = []
         
-        ## TODO: iterate through all of the landmarks in a world
-        
-        ## TODO: For each landmark
-        ## 1. compute dx and dy, the distances between the robot and the landmark
-        ## 2. account for measurement noise by *adding* a noise component to dx and dy
-        ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
-        ##    - Feel free to use the function self.rand() to help calculate this noise component
-        ##    - It may help to reference the `move` function for noise calculation
-        ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
-        ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
-        ##    as list.append([index, dx, dy]), this format is important for data creation done later
-        
-        ## TODO: return the final, complete list of measurements
-        
         for index, lmark in enumerate(self.landmarks):
             
+            lmark_x = lmark[0]
+            lmark_y = lmark[1]
+           
             # calculate dx, dx between the robot and landmark including noise component
-            dx = abs(self.x - lmark.x) + self.rand() * self.motion_nose
-            dy = abs(self.y - lmark.y) + self.rand() * self.motion_nose
+            dx = self.x - lmark_x + self.rand() * self.measurement_noise
+            dy = self.y - lmark_y + self.rand() * self.measurement_noise
             
-            # exclude landmarks that are out of range of robot sensors
-            if dx > self.measurement_range or dy > self.measurement_range:
-                continue
-                
-            measurements.append(index, dx, dy)
+            # include only landmarks that are in the range of robot sensors
+            if abs(dx) <= self.measurement_range and abs(dy) <= self.measurement_range:
+                measurements.append([index, dx, dy])
+        
         return measurements
 
 
